d5118a4df18f876d1221cec3a9a9fca8911af85c
jeff@osbzr.com <jeff@osbzr.com>
2013-10-07 23:42:20 +0800

    增加科目余额表

--- openerp7/openerp/addons/oecn_account_print/account.py
+++ openerp7/openerp/addons/oecn_account_print/account.py
@@ -2,8 +2,8 @@
 # __author__ = jeff@openerp.cn
 from osv import osv, fields
 from tools.translate import _
-
-
+from openerp import tools
+import openerp.addons.decimal_precision as dp
 
 class account_move_line(osv.osv):
     _inherit = 'account.move.line'
@@ -29,7 +29,6 @@ class account_move(osv.osv):
         'journal_id': lambda self, cr, uid, context:self.pool.get('account.journal').search(cr, uid, [('type', '=', 'general')], limit=1)[0],
     }
 
-account_move()
 
 class account_account(osv.osv):
     _inherit = 'account.account'
@@ -108,5 +107,59 @@ class account_account(osv.osv):
 
         return result
 
-account_account()
+class account_periodly(osv.osv):
+    _name = "account.periodly"
+    _description = "科目余额表"
+    _auto = False
 
+    def _compute_balances(self, cr, uid, ids, field_names, arg=None, context=None,
+                  query='', query_params=()):
+        all_periodly_lines = self.search(cr, uid, [], context=context)
+        all_companies = self.pool.get('res.company').search(cr, uid, [], context=context)
+        current_sum = dict((company, 0.0) for company in all_companies)
+        res = dict((id, dict((fn, 0.0) for fn in field_names)) for id in all_periodly_lines)
+        for record in self.browse(cr, uid, all_periodly_lines, context=context):
+            res[record.id]['starting_balance'] = current_sum[record.company_id.id] 
+            current_sum[record.company_id.id] += record.balance
+            res[record.id]['ending_balance'] = current_sum[record.company_id.id]
+        return res    
+
+    _columns = {
+        'fiscalyear_id': fields.many2one('account.fiscalyear', 'Fiscalyear', readonly=True),
+        'period_id': fields.many2one('account.period', '期间', readonly=True),
+        'account_id':fields.many2one('account.account','科目',readonly=True),
+        'debit': fields.float('借方', readonly=True),
+        'credit': fields.float('贷方', readonly=True),
+        'balance': fields.float('Balance', readonly=True),
+        'date': fields.date('Beginning of Period Date', readonly=True),
+        'starting_balance': fields.function(_compute_balances, digits_compute=dp.get_precision('Account'), string='期初余额', multi='balance'),
+        'ending_balance': fields.function(_compute_balances, digits_compute=dp.get_precision('Account'), string='期末余额', multi='balance'),
+        'company_id': fields.many2one('res.company', 'Company', readonly=True),
+    }
+
+    _order = 'date asc'
+
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'account_periodly')
+        cr.execute("""
+            create or replace view account_periodly as (
+            select
+                l.id as id,
+                p.fiscalyear_id as fiscalyear_id,
+                p.id as period_id,
+                l.account_id as account_id,
+                sum(l.debit) as debit,
+                sum(l.credit) as credit,
+                sum(l.debit-l.credit) as balance,
+                p.date_start as date,
+                am.company_id as company_id
+            from
+                account_move_line l
+                left join account_account a on (l.account_id = a.id)
+                left join account_move am on (am.id=l.move_id)
+                left join account_period p on (am.period_id=p.id)
+            where l.state != 'draft'
+            group by l.id, p.id, l.account_id, p.fiscalyear_id, p.date_start, am.company_id
+            )
+        """)
